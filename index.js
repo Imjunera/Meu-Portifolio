8a3d106856165e46bbde56a38a089f914e1e0603
Imjunera <eantoniodesouzajunior@gmail.com>
2025-02-19 20:36:58 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const links = document.querySelectorAll('nav ul li a');
     const sections = document.querySelectorAll('section');
     const header = document.querySelector('header');
+    const themeToggle = document.getElementById('theme-toggle');
 
-    for (const link of links) {
+    links.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
             const targetId = this.getAttribute('href').substring(1);
             const targetElement = document.getElementById(targetId);
 
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
+            sections.forEach(section => section.classList.remove('visible'));
+            setTimeout(() => {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+                targetElement.classList.add('visible');
+            }, 300);
         });
-    }
+    });
 
-    // Animações ao rolar a página
     window.addEventListener('scroll', function() {
         sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
@@ -29,11 +33,14 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
 
-        // Mostrar/ocultar a barra de navegação
         if (window.scrollY > 50) {
             header.style.top = '-100px';
         } else {
             header.style.top = '0';
         }
+    });
+
+    themeToggle.addEventListener('click', function() {
+        document.body.classList.toggle('dark-theme');
     });
 });
